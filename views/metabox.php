3955baa8bea4afb779d87418cfa733b60f0228d3
Kibru Demeke <kibdemta@gmail.com>
2022-04-08 21:05:46 +0300

    google map integration

--- views/metabox.php
+++ views/metabox.php
@@ -4,6 +4,13 @@
 $date_value = get_post_meta( $post->ID, 'date', true );
 $url_value = get_post_meta( $post->ID, 'url', true );
 $location_value = get_post_meta( $post->ID, 'location', true );
+$lat = 46.15242437752303;
+$lng = 2.7470703125;
+if($location_value){
+	$location_value = explode('|', $location_value);
+	$lat = $location_value[0];
+	$lng = $location_value[1];
+}
 
 
 wp_nonce_field(EVENTLISTING.'_nonce_action', EVENTLISTING.'_nonce'); 
@@ -22,10 +29,48 @@ wp_nonce_field(EVENTLISTING.'_nonce_action', EVENTLISTING.'_nonce');
 
 <tr>
     <th>
-    	<label for="location" class="">Location</label>
+    	<label for="location" class="">Location  </label>
     </th>
     <td>
-    	<input type="text" id="location" name="location" class="regular-text ltr" placeholder="" value="<?=esc_attr__( $location_value )?>">
+    	<input type="hidden" id="location" name="location" >
+    	<script type="text/javascript" src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBkmFAzmTPiOnVTRmfkaQACABtYcFCcjrI"></script>
+<script type="text/javascript" src="https://rawgit.com/Logicify/jquery-locationpicker-plugin/master/dist/locationpicker.jquery.js"></script>
+
+
+
+<script type="text/javascript">
+	$(function() {
+ 
+$('#us2').locationpicker({
+   location: {latitude:  <?=$lat?>, longitude: <?=$lat?>},   
+   radius: 0,
+   inputBinding: {
+      latitudeInput: $('#lat'),
+      longitudeInput: $('#lng'),
+      locationNameInput: $('#location')
+   },
+   enableAutocomplete: true,
+   onchanged: function(currentLocation, radius, isMarkerDropped) {
+   		document.getElementById('location').value = currentLocation.latitude+'|'+currentLocation.longitude
+    }
+});
+ 
+ 
+});
+
+</script>
+
+
+ Location: <input type="text" id="location" style="width: 200px"/>
+Lat <input type="text" id="lat" style="width: 200px"/>
+Long: <input type="text" id="lng" style="width: 200px"/>
+<div id="us2" style="height: 400px;"></div> 
+
+
+
+
+
     </td>
   </tr>
 
@@ -42,3 +87,4 @@ wp_nonce_field(EVENTLISTING.'_nonce_action', EVENTLISTING.'_nonce');
 
 
 </table>
+
